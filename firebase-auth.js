567ab5878a93bdafb53a35510f8f422b50a12ac4
Paulo Ricardo Gomes de oliveira <neoxpla54666@gmail.com>
2025-11-18 14:32:18 -0300

    Refactor Firebase auth functions for clarity

--- firebase-auth.js
+++ firebase-auth.js
@@ -1,46 +1,20 @@
-import { app } from "./firebase-config.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
-
-export const auth = getAuth(app);
-
-// Exemplo de cadastro
-export async function registrar(email, senha) {
-  return await createUserWithEmailAndPassword(auth, email, senha);
-}
-
-// Exemplo de login
-export async function entrar(email, senha) {
-  return await signInWithEmailAndPassword(auth, email, senha);
-}
-
-// Exemplo de logout
-export async function sair() {
-  return await signOut(auth);
-}
-
 // firebase-auth.js
+
 import { auth } from "./firebase-config.js";
 import {
-  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 
-export async function registrarUsuario(email, senha) {
-  return await createUserWithEmailAndPassword(auth, email, senha);
+export function loginUsuario(email, senha) {
+  return signInWithEmailAndPassword(auth, email, senha);
 }
-export async function loginUsuario(email, senha) {
-  return await signInWithEmailAndPassword(auth, email, senha);
+
+export function logoutUsuario() {
+  return signOut(auth);
 }
-export async function logoutUsuario() {
-  return await signOut(auth);
-}
+
 export function observarAuth(callback) {
   return onAuthStateChanged(auth, callback);
 }
