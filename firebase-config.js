e8433808a3f33611057a3e14708059dc46c9d0d0
Paulo Ricardo Gomes de oliveira <neoxpla54666@gmail.com>
2025-11-18 14:30:59 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,10 @@
-// Importar Firebase (versão modular)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
-
-// Configuração do Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyCW0Se5_lPtquxKMaryNyUHnAvoUmHkSCA",
-  authDomain: "virtual-mudar.firebaseapp.com",
-  projectId: "virtual-mudar",
-  storageBucket: "virtual-mudar.firebasestorage.app",
-  messagingSenderId: "984944463684",
-  appId: "1:984944463684:web:3134eb7719b68bf36f9111",
-  measurementId: "G-YFYEJX10DY"
-};
-
 // firebase-config.js
-// Versão modular via CDN
+// Configuração principal do Firebase
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-storage.js";
 
 const firebaseConfig = {
@@ -31,11 +17,11 @@ const firebaseConfig = {
   measurementId: "G-YFYEJX10DY"
 };
 
+// Inicializa o APP
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
-export const db = getFirestore(app);
+
+// Exporta serviços
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
-// Inicializar Firebase
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
