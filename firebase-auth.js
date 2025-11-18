824bd9148f3c99ec87e33c641a787719ee2b66ae
Paulo Ricardo Gomes de oliveira <neoxpla54666@gmail.com>
2025-11-18 13:43:20 -0300

    Add user registration and login functions

--- firebase-auth.js
+++ firebase-auth.js
@@ -22,3 +22,25 @@ export async function entrar(email, senha) {
 export async function sair() {
   return await signOut(auth);
 }
+
+// firebase-auth.js
+import { auth } from "./firebase-config.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+
+export async function registrarUsuario(email, senha) {
+  return await createUserWithEmailAndPassword(auth, email, senha);
+}
+export async function loginUsuario(email, senha) {
+  return await signInWithEmailAndPassword(auth, email, senha);
+}
+export async function logoutUsuario() {
+  return await signOut(auth);
+}
+export function observarAuth(callback) {
+  return onAuthStateChanged(auth, callback);
+}
